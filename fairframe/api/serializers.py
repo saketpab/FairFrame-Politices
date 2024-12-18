f674bbac9ef4b5cf25bfc77992dc4dc908047134
saketpab <saketp73@gmail.com>
2024-12-17 22:52:33 -0500

    hhh

--- fairframe/api/serializers.py
+++
@@ -1,8 +0,0 @@
-#this takes our models and translate it into a JSON response 
-from rest_framework import serializers
-from .models import Article
-
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('id','title', 'source', 'political_affiliation', 'posted_at' )
